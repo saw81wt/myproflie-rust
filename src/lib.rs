5618cc583dafc8128abb476939d118c40e19fb7f
前原宗太朗 <s.maehara81@icloud.com>
2022-01-09 23:31:15 +0900

    yarn導入

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,27 @@
 use seed::{prelude::*, *};
 
+fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
+    Model {}
+}
+
+struct Model {
+
+}
+
+enum Msg {
+
+}
+
+fn update(_: Msg, _: &mut Model, _: &mut impl Orders<Msg>) {
+
+}
+
+fn view(_: &Model) -> Vec<Node<Msg>> {
+    nodes![
+        header![C![""]]
+    ]
+}
+
 #[wasm_bindgen(start)]
 pub fn start() {
     console_error_panic_hook::set_once();
