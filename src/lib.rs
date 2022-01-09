dbf071884dd7a9992009047b457158256a0652d1
前原宗太朗 <s.maehara81@icloud.com>
2022-01-09 21:57:47 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+use seed::{prelude::*, *};
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    console_error_panic_hook::set_once();
+
+    let root_element = document()
+        .get_elements_by_class_name("todoapp")
+        .item(0)
+        .expect("element with the class `todoapp`");
+
+    App::start(root_element, init, update, view);
+}
